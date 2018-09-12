77c7aca59fb47c80cffdf2c0994ff4fcdb4f57fa
whitelon <happysensey@gmail.com>
2018-09-12 13:54:51 +0300

    Configure swagger to show comments

--- TestPushApp/Startup.cs
+++ TestPushApp/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -41,6 +43,11 @@ namespace TestPushApp
                         Email = "plysant@gmail.com"
                     }
                 });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
