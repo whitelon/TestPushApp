3ad9c4d87ca1d1791dd45cf15536d7234ba8dd98
whitelon <happysensey@gmail.com>
2018-09-12 14:42:50 +0300

    Added [Required] to Notification fields

--- TestPushApp/Models/Notification.cs
+++ TestPushApp/Models/Notification.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,16 +19,19 @@ namespace TestPushApp.Models
         /// <summary>
         /// App API Token for pushover notifications
         /// </summary>
+        [Required]
         public string ApiToken { get; set; }
 
         /// <summary>
         /// User Key to send pushover notifications to concrete user
         /// </summary>
+        [Required]
         public string UserKey { get; set; }
 
         /// <summary>
         /// Message to send in pushover notification
         /// </summary>
+        [Required]
         public string Message { get; set; }
 
     }
